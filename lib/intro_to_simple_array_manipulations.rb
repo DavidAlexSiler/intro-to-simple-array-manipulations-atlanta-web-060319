ca673217b8f2dd3dc21a2f1687ad3f0ac2926787
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 22:16:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,41 @@ def using_unshift(a, s)
   a.unshift(s)
 end
 
+def using_pop(a)
+  a.pop()
+end
 
-  
+def pop_with_args(a)
+  a.pop(2)
+end
+
+def using_shift(a)
+  a.shift()
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
+def using_concat(a, b)
+  a.concat(b)
+end
+
+def using_insert(a, e)
+  a.insert(e, 4)
+end
+
+def using_uniq(a)
+  a.uniq()
+end
+
+def using_flatten(a)
+  a.flatten()
+end
+
+def using_delete(a, s)
+  a.delete(s)
+end
+
+
   
